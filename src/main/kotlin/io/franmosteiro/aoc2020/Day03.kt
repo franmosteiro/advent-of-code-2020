008b12d8c2777674d1de2d4ca193de256aea2056
Fran Mosteiro <fran.mosteiro@holaluz.com>
2020-12-03 09:45:15 +0100

    Day03 - skeleton

---
+++ src/main/kotlin/io/franmosteiro/aoc2020/Day03.kt
@@ -0,0 +1,18 @@
+package io.franmosteiro.aoc2020
+
+/**
+ * Advent of Code 2020 - Day 3: Toboggan Trajectory -
+ * Problem Description: http://adventofcode.com/2020/day/3
+ */
+class Day03(list: List<String>) {
+
+    fun resolvePartOne(): Int {
+        throw IllegalStateException("No matches found")
+    }
+
+    fun resolvePartTwo(): Int {
+        throw IllegalStateException("No matches found")
+    }
+
+}
+
---
+++ src/test/kotlin/io/franmosteiro/aoc2020/Day03Test.kt
@@ -0,0 +1,67 @@
+package io.franmosteiro.aoc2020
+
+import io.franmosteiro.aoc2020.utils.Resources
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+/**
+ * Advent of Code 2020 - Day 3: Toboggan Trajectory -
+ * Problem Description: http://adventofcode.com/2020/day/3
+ */
+@DisplayName("Day 3")
+class Day03Test {
+
+    @Nested
+    @DisplayName("Part One")
+    inner class Part1 {
+        @Test
+        fun `Test example listOf`() {
+
+            // Given
+            val list = Resources.resourceAsList("testFile03.in")
+
+            // When
+            val answer = Day03(list).resolvePartOne()
+
+            // Then
+            //assertThat(answer).isEqualTo(2)
+        }
+
+        @Test
+        fun `Test input file Day03in`() {
+            // When
+            val answer = Day03(Resources.resourceAsList("Day03.in")).resolvePartOne()
+
+            // Then
+            //assertThat(answer).isEqualTo(396)
+        }
+    }
+
+    @Nested
+    @DisplayName("Part Two")
+    inner class Part2 {
+        @Test
+        fun `Test example input`() {
+
+            // Given
+            val list = Resources.resourceAsList("testFile03.in")
+
+            // When
+            val answer = Day03(list).resolvePartTwo()
+
+            // Then
+            //assertThat(answer).isEqualTo(1)
+        }
+
+        @Test
+        fun `Test input file Day03in`() {
+            // When
+            val answer = Day03(Resources.resourceAsList("Day03.in")).resolvePartTwo()
+
+            // Then
+            //assertThat(answer).isEqualTo(428)
+        }
+    }
+}
