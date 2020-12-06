532261f7ed494b754f64f41a5f3b9a26435dca2f
Fran Mosteiro <fran.mosteiro@holaluz.com>
2020-12-06 20:43:35 +0100

    day06 - skeleton

---
+++ src/main/kotlin/io/franmosteiro/aoc2020/Day06.kt
@@ -0,0 +1,19 @@
+package io.franmosteiro.aoc2020
+
+/**
+ * Advent of Code 2020 - Day 6: Custom Customs -
+ * Problem Description: http://adventofcode.com/2020/day/6
+ */
+class Day06(input: List<String>) {
+
+
+    fun resolvePartOne(): Int {
+        return 1
+    }
+
+    fun resolvePartTwo(): Int {
+        return 1
+    }
+
+}
+
---
+++ src/test/kotlin/io/franmosteiro/aoc2020/Day06Test.kt
@@ -0,0 +1,55 @@
+package io.franmosteiro.aoc2020
+
+import io.franmosteiro.aoc2020.utils.Resources
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+/**
+ * Advent of Code 2020 - Day 6: Custom Customs -
+ * Problem Description: http://adventofcode.com/2020/day/6
+ */
+@DisplayName("Day 6s")
+class Day06Test {
+
+    @Nested
+    @DisplayName("Part One")
+    inner class Part1 {
+        @Test
+        fun `Test example list`() {
+
+            // Given
+            val list = Resources.resourceAsList("testFile06.in")
+
+            // When
+            val answer = Day05(list).resolvePartOne()
+
+            // Then
+            assertThat(answer).isEqualTo(11)
+        }
+
+        @Test
+        fun `Test input file Day06in`() {
+            // When
+            val answer = Day06(Resources.resourceAsList("Day06.in")).resolvePartOne()
+
+            // Then
+            assertThat(answer).isEqualTo(976)
+        }
+    }
+
+    @Nested
+    @DisplayName("Part Two")
+    inner class Part2 {
+
+        @Test
+        fun `Test input file Day06sin`() {
+            // When
+            val answer = Day06(Resources.resourceAsList("Day05.in")).resolvePartTwo()
+
+            // Then
+            assertThat(answer).isEqualTo(685)
+        }
+    }
+}
