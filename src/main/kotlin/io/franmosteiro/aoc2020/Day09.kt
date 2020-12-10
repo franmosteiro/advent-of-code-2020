63b28b90179396de17a74e2440745f0e88c8d562
Fran Mosteiro <fran.mosteiro@holaluz.com>
2020-12-10 17:16:44 +0100

    day9 - skeleton

---
+++ src/main/kotlin/io/franmosteiro/aoc2020/Day09.kt
@@ -0,0 +1,14 @@
+package io.franmosteiro.aoc2020
+
+/**
+ * Advent of Code 2020 - Day 9: Encoding Error -
+ * Problem Description: http://adventofcode.com/2020/day/9
+ */
+class Day09(input: List<String>) {
+
+    fun resolvePartOne(): Int = -1
+
+    fun resolvePartTwo(): Int = -1
+
+}
+
---
+++ src/test/kotlin/io/franmosteiro/aoc2020/Day09Test.kt
@@ -0,0 +1,67 @@
+package io.franmosteiro.aoc2020
+
+import io.franmosteiro.aoc2020.utils.Resources
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+/**
+ * Advent of Code 2020 - Day 9: Encoding Error -
+ * Problem Description: http://adventofcode.com/2020/day/9
+ */
+@DisplayName("Day 9")
+class Day09Test {
+
+    @Nested
+    @DisplayName("Part One")
+    inner class Part1 {
+        @Test
+        fun `Test example input`() {
+
+            // Given
+            val list = Resources.resourceAsList("testFile09.in")
+
+            // When
+            val answer = Day08(list).resolvePartOne()
+
+            // Then
+            assertThat(answer).isEqualTo(127)
+        }
+
+        @Test
+        fun `Test input file Day09in`() {
+            // When
+            val answer = Day09(Resources.resourceAsList("Day09.in")).resolvePartOne()
+
+            // Then
+            assertThat(answer).isEqualTo(-1)
+        }
+    }
+
+    @Nested
+    @DisplayName("Part Two")
+    inner class Part2 {
+        @Test
+        fun `Test example input`() {
+
+            // Given
+            val list = Resources.resourceAsList("testFile09.in")
+
+            // When
+            val answer = Day09(list).resolvePartTwo()
+
+            // Then
+            assertThat(answer).isEqualTo(-1)
+        }
+
+        @Test
+        fun `Test input file Day09in`() {
+            // When
+            val answer = Day09(Resources.resourceAsList("Day09.in")).resolvePartTwo()
+
+            // Then
+            assertThat(answer).isEqualTo(-1)
+        }
+    }
+}
